5a431b9964533908f367bf8ec21ebbb854b81e93
William Mattsson <william.mattsson@iths.se>
2025-01-07 14:59:51 +0100

    Add dotenv support and fixed contentful connection

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+/* .env för att gömma nycklar */
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
     siteMetadata: {
         title: `Gatsby Portfolio`,
@@ -10,8 +16,8 @@ module.exports = {
         {
             resolve: 'gatsby-source-contentful',
             options: {
-                accessToken: 'accessToken',
-                spaceId: ''
+                accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+                spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID
             }
         },
         'gatsby-plugin-image',
