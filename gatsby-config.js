1b8f35579a7e051ef1bddcd8d6b7890d718ac8fc
William Mattsson <william.mattsson@iths.se>
2025-01-07 14:34:04 +0100

    Added plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,21 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "accessToken",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
-};
+    siteMetadata: {
+        title: `Gatsby Portfolio`,
+        siteUrl: `https://www.yourdomain.tld`
+    },
+    plugins: [
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                accessToken: 'accessToken',
+                spaceId: ''
+            }
+        },
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        'gatsby-transformer-remark'
+    ]
+}
