e3b0828a3bc4f1191ebc92584bb910eb0787bdcc
William Mattsson <william.mattsson@iths.se>
2025-01-08 16:59:03 +0100

    Add SEO component to About and Contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const AboutPage = () => {
     return (
@@ -8,5 +9,7 @@ const AboutPage = () => {
         </Layout>
     )
 }
+
+export const Head = () => <Seo title={'About'}></Seo>
 
 export default AboutPage
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const ContactPage = () => {
     return (
@@ -8,5 +9,7 @@ const ContactPage = () => {
         </Layout>
     )
 }
+
+export const Head = () => <Seo title={'Contact'}></Seo>
 
 export default ContactPage
