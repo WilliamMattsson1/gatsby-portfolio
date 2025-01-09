fb3be6e8c21164def39a78436e992b476d936ebc
William Mattsson <william.mattsson@iths.se>
2025-01-09 10:06:08 +0100

    Add project template with GraphQL query and SEO component to show content

---
+++ src/templates/project.js
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
+
+const Project = ({ data }) => {
+    const { title, githubLink, projectLink, toolsUsed, image, description } =
+        data.contentfulPortfolioItem
+    return (
+        <Layout>
+            <h1>{title}</h1>
+            <GatsbyImage
+                image={image.gatsbyImageData}
+                alt={image.description || title}
+            />
+            <p>{description.description}</p>
+            <p>{toolsUsed.join(' | ')}</p>
+            <a href={githubLink}>Github</a>
+            <a href={projectLink}>Project</a>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query ($slug: String!) {
+        contentfulPortfolioItem(slug: { eq: $slug }) {
+            title
+            githubLink
+            projectLink
+            toolsUsed
+            description {
+                description
+            }
+            image {
+                gatsbyImageData(width: 500, height: 500, placeholder: BLURRED)
+                description
+            }
+        }
+    }
+`
+
+export const Head = ({ data }) => (
+    <Seo title={`${data.contentfulPortfolioItem.title}`}></Seo>
+)
+
+export default Project
