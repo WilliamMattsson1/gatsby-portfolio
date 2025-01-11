5f3303e6bfbceacfdd0c36dec2b0cc2660128072
William Mattsson <william.mattsson@iths.se>
2025-01-11 15:09:27 +0100

    Added 404 page with link to home

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,25 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import '../styles/404.css'
+import Seo from '../components/seo'
 
 const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    return (
+        <Layout>
+            <div className="not-found-container">
+                <h1 className="not-found-title">404</h1>
+                <p className="not-found-description">
+                    Oops! The page you are looking for does not exist.
+                </p>
+                <Link to="/" className="btn not-found-btn">
+                    Go Home
+                </Link>
+            </div>
+        </Layout>
+    )
 }
 
-export default NotFoundPage
+export const Head = () => <Seo title={'Not found'}></Seo>
 
-export const Head = () => <title>Not found</title>
+export default NotFoundPage
